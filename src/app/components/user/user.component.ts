61678922807a9b6fe7f50d19d61ee4ce2f2d44e1
AndrzejKnit <andrzej.knitter@gmail.com>
2019-01-24 15:32:36 +0100

    Lesson 16: Generating Components & OnInit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { User } from '../../models/User';
 
@@ -9,23 +9,27 @@ import { User } from '../../models/User';
 
 })
 
-export class UserComponent {
+export class UserComponent implements OnInit {
     // Properties
     user: User;
 
 
     // Methods
     constructor() {
-        this.user = {
-            firstName: 'John',
-            lastName: 'Doe',
-            age: 30,
-            address: {
-                street: '50 Main st',
-                city: 'Boston',
-                state: 'MA'
+
+    }
+
+    ngOnInit() {
+    this.user = {
+                firstName: 'John',
+                lastName: 'Doe',
+                age: 30,
+                address: {
+                    street: '50 Main st',
+                    city: 'Boston',
+                    state: 'MA'
+                }
             }
-        }
     }
 }
 
