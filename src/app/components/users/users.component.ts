bc3025130f3c5a056ff0ea363d045ad0dbe1e03c
AndrzejKnit <andrzej.knitter@gmail.com>
2019-01-28 14:02:04 +0100

    Lesson 24: Clean up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,8 +11,6 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
 
   constructor() { }
 
@@ -29,9 +27,7 @@ export class UsersComponent implements OnInit {
               city: 'Boston',
               state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/3',
           isActive: true,
-          balance: 100,
           registered: new Date('01.02.2018 08:30:00')
       },
       {
@@ -43,9 +39,7 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'MA'
         },
-        image: 'http://lorempixel.com/600/600/people/2',
         isActive: true,
-        balance: 200,
           registered: new Date('08.02.2018 09:30:00')
     },
     {
@@ -57,37 +51,14 @@ export class UsersComponent implements OnInit {
           city: 'Boston',
           state: 'MA'
       },
-      image: 'http://lorempixel.com/600/600/people/1',
       isActive: false,
-      balance: 300,
-          registered: new Date('23.04.2018 12:30:00')
+          registered: new Date('12.04.2018 12:30:00')
   }
       ];
       this.loaded = true;
-
-
-//  this.addUser({
-//       firstName: 'David',
-//       lastName: 'Hey'
-//   });
-      this.setCurrentClasses();
-      this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
     }
-    setCurrentClasses() {
-      this.currentClasses = {
-        'btn-success': this.enableAdd,
-        'big-text': this.showExtended
-      };
-    }
-    setCurrentStyles() {
-      this.currentStyles = {
-        'padding-top': this.showExtended ? '0' : '40px',
-        'font-size': this.showExtended ? '' : '40px'
-      };
-    }
-
 }
