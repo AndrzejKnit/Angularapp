52616b8ae750230dc5f66eb1f029381a3d194926
AndrzejKnit <andrzej.knitter@gmail.com>
2019-02-04 16:17:00 +0100

    Lesson 37: Event Emitter & Output

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -16,7 +16,7 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts() : Observable<Post[]> {
+  getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postsUrl);
   }
 
